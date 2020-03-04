293df310a4ceafe1fd85086d7209c0bf0e213bca
venkivijay <venki96@hotmail.com>
2020-03-04 10:03:15 +0530

    Added reverse of a number program

---
+++ Reverse_Number/Question.md
@@ -0,0 +1,19 @@
+## Problem Statement
+
+Write a Java program to reverse a given number
+
+### Condition
+Input will only be a positive interger and will not have any characters
+
+## Input
+
+    12345
+
+## Output
+
+    54321
+
+## Explanation
+
+> Number modulo 10 will give the last digit as result. (ie., 12345 % 10 = 5).
+> Again dividing the same number by 10 will give the remaining digits except last one.
---
+++ Reverse_Number/Solution.java
@@ -0,0 +1,19 @@
+package Reverse_Number;
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int digit = sc.nextInt();
+        sc.close();
+        System.out.print("Reverse of the number is ");
+        if(digit == 0)
+            System.out.print(0);
+        else {
+            while(digit!=0){
+                System.out.print(digit%10);
+                digit /= 10;
+            }
+        }
+    }
+}
