a4ee7ec4881feb2115260d854668b4e846a14284
venkivijay <venki96@hotmail.com>
2020-07-09 13:26:02 +0530

    Analytics

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import VueAnalytics from "vue-analytics";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "UA-152753785-1",
+  router,
+});
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
